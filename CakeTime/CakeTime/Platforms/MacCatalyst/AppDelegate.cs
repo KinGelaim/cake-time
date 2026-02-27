0d02520cc7d1532c87b431a35a354454c6b43e74
KinGelaim <89123232156@mail.ru>
2026-02-27 11:40:30 +0500

    Added a blank for a future project with preliminary images and fonts

---
+++ CakeTime/CakeTime/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CakeTime;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
